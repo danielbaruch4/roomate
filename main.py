62bd9152a4e9e2fac479aa15f4f14d29d5e650d0
danielbaruch4 <bettermetavers@gmail.co.il>
2023-02-08 17:50:15 +0200

    sadas

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from typing import Union
 
 from fastapi import FastAPI
 
-import uvicorn
-
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
 
 # if __name__ == "__main__":
 #     uvicorn.run('server:app',port=5000,host="0.0.0.0",reload=True)
