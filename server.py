511cf0c887520f8704eae7246cb5b592950af3a5
danielbaruch4 <bettermetavers@gmail.co.il>
2023-02-08 17:48:39 +0200

    changed to fast api

--- server.py
+++ server.py
@@ -1,18 +1,36 @@
-from flask import Flask
+from typing import Union
+
 from fastapi import FastAPI
-print("flask server")
+
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
-app = Flask(__name__)
-print(__name__)
 
-@app.route('/')
-def test():
-    return 'HOME PAGErrrsdasdadasr1'
+# if __name__ == "__main__":
+#     uvicorn.run('server:app',port=5000,host="0.0.0.0",reload=True)
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
+
+
+# from flask import Flask
+# print("flask server")
+
+
+# app = Flask(__name__)
+# print(__name__)
+
+# @app.route('/')
+# def test():
+#     return {'data':[1,2,3]}
+
+# @app.route('/hello')
+# def hello():
+#     return 'Hello, World!'
     
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=False)
+# if __name__ == '__main__':
+#   app.run(host='0.0.0.0', debug=False)
