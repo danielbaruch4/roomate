7a20288ce7b4b2248708c1cd88a814085f60e604
danielbaruch4 <bettermetavers@gmail.co.il>
2023-02-08 17:11:01 +0200

    server sample route

--- server.py
+++ server.py
@@ -1,3 +1,18 @@
 from flask import Flask
 from fastapi import FastAPI
 print("flask server")
+
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def test():
+    return 'HOME PAGErrrsdasdadasr1'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+    
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=False)
